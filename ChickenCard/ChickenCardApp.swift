318e812a1a693ead74d73398ba663f156317bc4e
phanngan811 <s3863893@rmit.edu.vn>
2022-08-17 18:37:01 +0700

    Initial Commit

---
+++ ChickenCard/ChickenCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChickenCardApp.swift
+//  ChickenCard
+//
+//  Created by phan thi thu ngan on 17/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChickenCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChickenCardUITests/ChickenCardUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ChickenCardUITestsLaunchTests.swift
+//  ChickenCardUITests
+//
+//  Created by phan thi thu ngan on 17/08/2022.
+//
+
+import XCTest
+
+class ChickenCardUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
